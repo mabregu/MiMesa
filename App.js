347fbccb31ed06e22f1e3d3f5725245404a1fc73
N. Martin Abregu <nmabregu@gmail.com>
2019-09-25 12:45:16 -0300

    caja con resto

--- App.js
+++ App.js
@@ -1,33 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import RestoBox from './RestoBox';
 
-const image = 'https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2018/06/08/15284709731053.jpg'
-const name = 'Comida de Sueños'
-const likes = 200
-const comments = 140
+//export default function App() {
+export default class App extends Component {
+  render() {
+    const restos = {
+      image: 'https://blog.restorando.com/wp-content/uploads/2017/10/osso.jpg',
+      name: 'Comida de Sueños',
+      likes: 200,
+      comments: 140,
+    }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.restoBox}>
-        <Image style={styles.image} source={{uri: image}} />
-        <View style={styles.info}>
-          <Text style={styles.name}>{name}</Text>
-          <View style={styles.row}>
-            <View style={styles.iconContainer}>
-              <Ionicons name="md-heart-empty" size={32} color="gray" />
-              <Text style={styles.count}>{likes}</Text>
-            </View>
-            <View style={styles.iconContainer}>
-              <Ionicons name="md-chatbubbles" size={32} color="gray" />
-              <Text style={styles.count}>{comments}</Text>
-            </View>
-          </View>
-        </View>
+    return (
+      <View style={styles.container}>
+        <RestoBox restos={restos} />
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -35,37 +26,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'lightgray',
     paddingTop: 50,
-  },
-  image: {
-    width: 150,
-    height: 150,
-  },
-  restoBox: {
-    backgroundColor: 'white',
-    flexDirection: 'row',
-  },
-  info: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  name: {
-    fontSize: 20,
-    marginTop: 10,
-    color: '#333'
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 40,
-    marginTop: 15,
-  },
-  iconContainer: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  count: {
-    color: 'gray'
   }
 });
---
+++ RestoBox.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+//const { image, name, likes, comments } = this.props.restos
+export default class RestoBox extends Component {
+//export default function RestoBox() {
+  render() {
+    const { image, name, likes, comments } = this.props.restos
+    return (
+      <View style={styles.restoBox}>
+        <Image style={styles.image} source={{uri: image}} />
+        <View style={styles.info}>
+          <Text style={styles.name}>{name}</Text>
+          <View style={styles.row}>
+            <View style={styles.iconContainer}>
+              <Ionicons name="md-heart-empty" size={32} color="gray" />
+              <Text style={styles.count}>{likes}</Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <Ionicons name="md-chatbubbles" size={32} color="gray" />
+              <Text style={styles.count}>{comments}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 150,
+    height: 150,
+  },
+  restoBox: {
+    margin: 5,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    shadowColor: 'black',
+    shadowOpacity: .2,
+    shadowOffset: {
+      height: 1,
+      width: -2,
+    },
+    elevation: 2,
+  },
+  info: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  name: {
+    fontSize: 20,
+    marginTop: 10,
+    color: '#333'
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 40,
+    marginTop: 15,
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  count: {
+    color: 'gray'
+  }
+});
